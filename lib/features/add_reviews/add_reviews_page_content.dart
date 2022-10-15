1472ab663813cb973a92a33c288410bb90a38d77
Katarzyna Kozlowska <katarzyna.kozlowska@gmail.com>
2022-10-15 16:02:13 +0200

    Add AddReview page content

---
+++ lib/features/add_reviews/add_reviews_page_content.dart
@@ -0,0 +1,64 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class AddReview extends StatefulWidget {
+  const AddReview({super.key});
+
+  @override
+  State<AddReview> createState() => _AddReviewState();
+}
+
+class _AddReviewState extends State<AddReview> {
+  var movieTitle = '';
+  var movieRating = 1.0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+            child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Padding(
+          padding: EdgeInsets.all(20.0),
+          child: TextField(
+            decoration: InputDecoration(hintText: 'Movie title'),
+            onChanged: (newValue) {
+              setState(() {
+                movieTitle = newValue;
+              });
+            },
+          ),
+        ),
+        const SizedBox(
+          height: 20.0,
+        ),
+        Slider(
+          value: movieRating,
+          onChanged: (newValue) {
+            setState(
+              () {
+                movieRating = newValue;
+              },
+            );
+          },
+          min: 1.0,
+          max: 10.0,
+          divisions: 10,
+          label: movieRating.toString(),
+        ),
+        ElevatedButton(
+            onPressed: movieTitle.isEmpty
+                ? null
+                : () {
+                    FirebaseFirestore.instance.collection('reviews').add({
+                      'title': movieTitle,
+                      'rating': movieRating,
+                    });
+                  },
+            child: Text('Add'))
+      ],
+    )));
+  }
+}
--- lib/features/reviews/reviews_page_content.dart
+++ lib/features/reviews/reviews_page_content.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:curlzzz_new/features/add_reviews/add_reviews_page_content.dart';
 import 'package:flutter/material.dart';
 
 class ReviewsPage extends StatelessWidget {
@@ -8,13 +9,22 @@ class ReviewsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (_) => const AddReview(),
+            ),
+          );
+        },
         child: Icon(
           Icons.add,
         ),
       ),
       body: StreamBuilder<QuerySnapshot>(
-          stream: FirebaseFirestore.instance.collection('reviews').snapshots(),
+          stream: FirebaseFirestore.instance
+              .collection('reviews')
+              .orderBy('rating', descending: true)
+              .snapshots(),
           builder: (context, snapshot) {
             if (snapshot.hasError) {
               return Text('Error');
@@ -25,17 +35,26 @@ class ReviewsPage extends StatelessWidget {
             final documents = snapshot.data!.docs;
             return ListView(children: [
               for (final document in documents) ...[
-                Container(
-                    padding: EdgeInsets.all(20.0),
-                    margin: EdgeInsets.all(10.0),
-                    color: Color.fromARGB(255, 243, 177, 198),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        Text(document['title']),
-                        Text(document['rating'].toString())
-                      ],
-                    ))
+                Dismissible(
+                  key: ValueKey(document.id),
+                  onDismissed: (_) {
+                    FirebaseFirestore.instance
+                        .collection('reviews')
+                        .doc(document.id)
+                        .delete();
+                  },
+                  child: Container(
+                      padding: EdgeInsets.all(20.0),
+                      margin: EdgeInsets.all(10.0),
+                      color: Color.fromARGB(255, 243, 177, 198),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Text(document['title']),
+                          Text(document['rating'].toString())
+                        ],
+                      )),
+                )
               ]
             ]);
           }),
